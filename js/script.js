72cfdcc4dd57361859c95cfd9d36c297a1f62237
Kevin Gonzales <kv.gonzales56@gmail.com>
2021-03-07 21:04:26 -0700

    edits

--- js/script.js
+++ js/script.js
@@ -23,13 +23,13 @@ function Image(image, quote, tags, color) {
         $(".images").prepend(container);
     }
 }
-//"images/bulldog.jpg"
-//"images/chowchow.jpg"
 
 var images = [
 	new Image("images/hot1.jpg", "Heart of Tyre", ["HeartofTyre", "Single"], "#ff726f"),
 	new Image("images/hot2.jpg", "Heart of Tyre", ["HeartofTyre", "Single"], "#ff726f"),
 	new Image("images/hot3.jpg", "Heart of Tyre", ["HeartofTyre", "Group"], "#ff726f"),
+    new Image("images/sot1.jpg", "Shadow of Tyre", ["ShadowofTyre", "Group"], "#fd71a8"),
+    new Image("images/sot2.jpg", "Shadow of Tyre", ["ShadowofTyre", "Single"], "#fd71a8"),
     new Image("images/sas1.jpg", "Steel and Silence", ["SteelandSilence", "Single"], "#6fcdff"),
     new Image("images/sas2.jpg", "Steel and Silence", ["SteelandSilence", "Group"], "#6fcdff"),
     new Image("images/sc1.jpg", "ShatteredCrowns", ["ShatteredCrowns", "Group"], "#e6d088"),
